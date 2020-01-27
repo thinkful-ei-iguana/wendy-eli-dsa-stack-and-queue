54b2f2ef0649a758c3a25127b64a0a9185870f02
user name <user@example.com>
2020-01-27 12:07:23 -0500

    problems 3 / 5

--- stack-drills.js
+++ stack-drills.js
@@ -28,4 +28,51 @@ function display(stack) {
   return stack.top.data;
 }
 
-display(starTrek);
+// display(starTrek);
+
+// palindrome
+const isPalindrome = s => {
+  s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+  // Your code goes here
+  const stack = new Stack();
+
+  s.split("").forEach(l => {
+    stack.push(l);
+  });
+
+  let acc = "";
+
+  while (stack.top != null) {
+    acc += stack.top.data;
+    stack.pop();
+  }
+
+  return acc == s;
+};
+
+// True, true, true, false
+// console.log(isPalindrome("dad"));
+// console.log(isPalindrome("A man, a plan, a canal: Panama"));
+// console.log(isPalindrome("1001"));
+// console.log(isPalindrome("Tauhida"));
+
+// 5. sort stack
+
+const sortStack = (stack, item) => {
+  const stack2 = new Stack();
+
+  // if the new node is smaller than the existing top,
+  // then it gets pushed to that stack.
+  // otherwise, push to stack 2
+  if (stack.top.data > item) {
+    stack.push(item);
+  } else stack2.push(item);
+
+  return stack;
+};
+
+const testStack = new Stack();
+testStack.push("4");
+testStack.push("3");
+
+console.log(sortStack(testStack, "2"));
